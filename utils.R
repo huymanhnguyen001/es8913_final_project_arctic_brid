d8106300966e72f838185844beb8d0d231e45485
Manh Huy Nguyen <huy.manh.nguyen@ryerson.ca>
2022-04-25 15:08:23 -0400

    Function for replacing zero with NA values for plotting scatter plot

--- utils.R
+++ utils.R
@@ -1,16 +1,8 @@
+# Function for replacing zero with NA values for plotting scatter plot
 zero_it_out <- function (data, col, options) {
   if (options == "by_NA") {
     for (col in col) {
       data[,col][data[,col] == 0] <- NA
-    }
-    return(data)
-  }
-  if (options == "LOD") {
-    data <- as.data.frame(data)
-    for (col in col) {
-      data[,col][data[,col] == 0] <- runif(sum(data[,col] == 0),
-                                                     min = 0.0145000,
-                                                     max = 0.0155000)
     }
     return(data)
   }
